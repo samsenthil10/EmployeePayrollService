03fcc77bb5b5c9a0141d483face6d2ab3c1ef599
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-30 02:38:38 +0530

    Refactored: To use proper naming convention for main class

--- src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayrollMain.java
+++ src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayrollMain.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 import com.bridgelabz.employeepayrollservice.EmployeePayrollService.IOService;
 
-public class EmployeePayrollServiceMain {
+public class EmployeePayrollMain {
 	
 	public static void main(String[] args) {
 
